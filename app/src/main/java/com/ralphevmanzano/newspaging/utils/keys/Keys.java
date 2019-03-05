bee3c31c0ef42918d768d0dceaecb2cf00e078cd
Ralph Manzano <ralphevmanzano@gmail.com>
2019-03-05 20:03:48 +0800

    Update Keys.java

--- app/src/main/java/com/ralphevmanzano/newspaging/utils/keys/Keys.java
+++ app/src/main/java/com/ralphevmanzano/newspaging/utils/keys/Keys.java
@@ -1,5 +1,5 @@
 package com.ralphevmanzano.newspaging.utils.keys;
 
 public class Keys {
-    public static final String NEWS_API_KEY = "c8b8bea1a52340278d6dee338bdaf09b";
+    public static final String NEWS_API_KEY = "";
 }
